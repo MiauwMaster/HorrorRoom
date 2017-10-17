c5eed1b8649225e26da867b1ab4a35af3109137e
Nikkes <nick.westerdijk@gmail.com>
2017-10-17 13:06:07 +0200

    Menu update
    Onderdelen toegevoegd

--- js/Menu.js
+++ js/Menu.js
@@ -3,10 +3,20 @@
  * by Nick & Tobias
  */
 
-var gui = new dat.GUI({
-    height : 5 * 32 - 1
-});
 
-var params = {
-    spiders : 2
+var Choices = function() {
+    this.renderspider = false;
+    this.renderclaustrofobia = false;
+    this.renderdolls = false;
+    this.renderdarkness = false;
+    // Define render logic ...
+};
+
+window.onload = function() {
+    var text = new Choices();
+    var gui = new dat.GUI();
+    gui.add(text, 'renderspider');
+    gui.add(text, 'renderclaustrofobia');
+    gui.add(text, 'renderdolls');
+    gui.add(text, 'renderdarkness');
 };
